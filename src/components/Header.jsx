c6320d8f958ec795ed6b8a9f79d1e20b6e274df4
IlliaMatushchak <coldnight100@gmail.com>
2025-07-24 21:07:09 +0300

    Add normalize.css & global styles. Create component Header and its styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,40 @@
-function Header() {
-  return null;
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/Header.css";
+import cartImg from "../assets/images/cart.svg";
+import avatarImg from "../assets/images/avatar.png";
+
+function Header({ userName = "NoName" }) {
+  return (
+    <>
+      <header className="header flex fancy-background">
+        <h1>Books shop</h1>
+
+        <nav className="flex">
+          <Link to="/cart" className="cart-link">
+            <img src={cartImg} width="50px" alt="Cart" />
+            <span>5</span>
+          </Link>
+
+          <Link to="/">Sign-out</Link>
+
+          <div className="user flex">
+            <Link to={"/"}>
+              <img
+                className="user-avatar"
+                src={avatarImg}
+                width="50px"
+                alt="Avatar"
+              />
+            </Link>
+            <Link to={"/"} className="user-name">
+              {userName}
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Header;
