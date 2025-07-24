05389a049bbc5e6eb1d5149340d2169d2ee69855
IlliaMatushchak <coldnight100@gmail.com>
2025-07-24 21:47:54 +0300

    Create Footer component and its styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+import "../assets/styles/Footer.css";
+
 function Footer() {
-  return null;
+  return (
+    <footer className="fancy-background">
+      <a href="https://github.com/IlliaMatushchak" target="_blank">
+        Created by <strong>Illia Matushchak</strong>
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
