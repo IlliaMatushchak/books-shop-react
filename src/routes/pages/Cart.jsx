77d0b0257becc99fe9a2cbedbaca8e6439f48ae1
IlliaMatushchak <coldnight100@gmail.com>
2025-08-08 15:14:20 +0300

    Create page Cart and its styles

--- src/routes/pages/Cart.jsx
+++ src/routes/pages/Cart.jsx
@@ -1,5 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
+import "../../assets/styles/Cart.css";
+import cartImg from "../../assets/images/cart.svg";
+
 function Cart() {
-  return null;
+  const { cart, setCart } = useCart();
+  const isEmpty = !cart.length;
+
+  return (
+    <div className="cart fancy-background">
+      <button
+        className="btn-purchase"
+        onClick={() => {
+          setCart([]);
+        }}
+        disabled={isEmpty}
+      >
+        Purchase
+      </button>
+
+      {isEmpty ? (
+        <div className="empty-cart-container">
+          <img src={cartImg} alt="Empty cart" />
+          <p>Cart is empty!</p>
+        </div>
+      ) : (
+        <>
+          {cart.map(({ id, orderedCount, price, title }) => {
+            return (
+              <div key={id} className="product-info flex">
+                <Link to={`/specific-book/${id}`}>{title}</Link>
+                <span>
+                  Count: {orderedCount} / ${(price * orderedCount).toFixed(2)}
+                </span>
+              </div>
+            );
+          })}
+          <p>
+            Total price, $
+            {cart
+              .reduce(
+                (acc, { orderedCount, price }) => acc + orderedCount * price,
+                0
+              )
+              .toFixed(2)}
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Cart;
