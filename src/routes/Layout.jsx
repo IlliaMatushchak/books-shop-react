35669bb74609c7efb651bda83962c682fdb7a54e
IlliaMatushchak <coldnight100@gmail.com>
2025-08-14 21:32:12 +0300

    Add scrolling to top when changing route to fix bug saving scroll between pages

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -7,12 +7,17 @@ import ScrollToTopButton from "../components/ScrollToTopButton";
 
 function Layout({ userNameState, isLoggedInState }) {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (!isLoggedInState.isLoggedIn) {
       navigate("/");
     }
   }, [isLoggedInState.isLoggedIn, navigate]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <>
