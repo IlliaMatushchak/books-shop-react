c0374dedd96265609a91ca776b718dc553fcab1a
IlliaMatushchak <coldnight100@gmail.com>
2025-11-04 18:26:13 +0200

    Implement PrivateRoute component with role-based navigation & add PublicRoute component

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,15 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 
-export default function PrivateRoute({ children, allowedRoles }) {
+export default function PublicRoute({ children }) {
   const { user, isLoggedIn } = useAuth();
   const role = user?.role?.toLowerCase();
 
-  if (!isLoggedIn) {
-    return <Navigate to="/" replace />;
-  }
-
-  if (allowedRoles && !allowedRoles.includes(role)) {
+  if (isLoggedIn) {
     if (role === "admin") return <Navigate to="/admin" replace />;
     if (role === "user") return <Navigate to="/shop" replace />;
   }
