7b60b0aafe6b73b9e6a80210e222c995e455fa01
IlliaMatushchak <coldnight100@gmail.com>
2025-11-06 20:42:15 +0200

    Add Profile page & its styles

---
+++ src/routes/pages/Profile/Profile.jsx
@@ -0,0 +1,175 @@
+import { useEffect, useState } from "react";
+import { ProfileService } from "../../../services/profileService";
+import Loader from "../../../components/Loader/Loader";
+import "./Profile.css";
+
+function validateForm(form) {
+  // Needs improvement !!!!!!!!!!!!!!!!!
+  return form?.email?.length >= 4 && form?.email?.length <= 16;
+}
+
+export default function Profile() {
+  const [profile, setProfile] = useState(null);
+  const [form, setForm] = useState({
+    username: "",
+    role: "",
+    email: "",
+    phoneNumber: "",
+    gender: "MALE",
+  });
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadProfile() {
+      try {
+        setLoading(true);
+        const data = await ProfileService.getProfile();
+        setProfile(data);
+        setForm(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProfile();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const removeNotification = () => {
+    setError(null);
+    setMessage("");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    removeNotification();
+    if (validateForm(form)) {
+      try {
+        setLoading(true);
+        const data = await ProfileService.updateProfile(form);
+        setProfile(data);
+        setEditing(false);
+        setMessage("Profile updated!");
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      setError("Invalid form data");
+    }
+  };
+
+  if (loading && !profile) return <Loader />;
+
+  return (
+    <div className="profile-container">
+      <form className="fancy-background" onSubmit={handleSubmit}>
+        <h2 className="">My profile</h2>
+        <label htmlFor="user-name">User name</label>
+        <input
+          id="user-name"
+          type="text"
+          name="username"
+          value={form?.username || ""}
+          disabled
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={form?.email || ""}
+          onChange={handleChange}
+          required
+          disabled={!editing}
+        />
+
+        <label htmlFor="phone-number">Phone Number</label>
+        <input
+          id="phone-number"
+          type="tel"
+          name="phoneNumber"
+          placeholder="Phone Number"
+          value={form?.phoneNumber || ""}
+          onChange={handleChange}
+          required
+          disabled={!editing}
+        />
+
+        <label htmlFor="gender">Gender</label>
+        <select
+          id="gender"
+          name="gender"
+          value={form?.gender || ""}
+          onChange={handleChange}
+          disabled={!editing}
+        >
+          <option value="MALE">Male</option>
+          <option value="FEMALE">Female</option>
+        </select>
+
+        <label htmlFor="role">Role</label>
+        <input
+          id="role"
+          type="text"
+          name="role"
+          value={form?.role || ""}
+          disabled
+        />
+
+        <hr />
+
+        {!editing ? (
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              setEditing(true);
+              removeNotification();
+            }}
+          >
+            Edit
+          </button>
+        ) : (
+          <>
+            <button type="submit" disabled={loading}>
+              Save
+            </button>
+            <button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                setEditing(false);
+                setForm(profile ?? form);
+                removeNotification();
+              }}
+            >
+              Cancel
+            </button>
+          </>
+        )}
+      </form>
+      {error && (
+        <p className="alert error" role="alert">
+          {error}
+        </p>
+      )}
+      {message && (
+        <p className="alert message" role="alert">
+          {message}
+        </p>
+      )}
+    </div>
+  );
+}
