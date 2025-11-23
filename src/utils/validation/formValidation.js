dada95cde03eae35ba68ccdcb903415b986f038a
IlliaMatushchak <coldnight100@gmail.com>
2025-11-23 12:44:18 +0200

    Code refactoring

--- src/utils/validation/formValidation.js
+++ src/utils/validation/formValidation.js
@@ -1,10 +1,8 @@
 import Validator from "./validator/validator";
 
 function validateForm(form, config) {
-  if (!form) throw new Error("No data found for validation " + form);
   const validator = new Validator(config);
-  let result = validator.validate(form);
-  return result;
+  return validator.validate(form);
 }
 
 export function validateLoginForm(form) {
