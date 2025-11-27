18ebc3a7ecd98957414bcecbc8d32f0b6d8a9863
IlliaMatushchak <coldnight100@gmail.com>
2025-11-27 21:21:09 +0200

    Refactoring Cart.jsx (creating CartItemsList.jsx and CartItem.jsx)

--- src/routes/pages/Cart/Cart.jsx
+++ src/routes/pages/Cart/Cart.jsx
@@ -1,13 +1,12 @@
-import React, { memo } from "react";
-import { Link } from "react-router-dom";
+import { memo } from "react";
 import { useCart } from "../../../contexts/CartContext";
 import "./Cart.css";
 import cartImg from "../../../assets/images/cart.svg";
+import CartItemsList from "../../../containers/CartItemsList/CartItemsList";
 
 const Cart = memo(function Cart() {
-  console.log("Cart render");
-  const { cart, totalPrice, clearCart, removeFromCart } = useCart();
-  const isEmpty = !cart.length;
+  const { totalPrice, totalCount, clearCart } = useCart();
+  const isEmpty = totalCount === 0;
 
   return (
     <div className="cart fancy-background">
@@ -27,27 +26,10 @@ const Cart = memo(function Cart() {
         </div>
       ) : (
         <>
-          {cart.map(({ productId, quantity, book: { price, title } }) => {
-            return (
-              <div key={productId} className="product-info flex">
-                <Link to={`/specific-book/${productId}`}>{title}</Link>
-                <span>
-                  Count: {quantity} / ${(price * quantity).toFixed(2)}
-                </span>
-                <button
-                  type="button"
-                  className="btn-text btn-effect-3d"
-                  aria-label="Remove from cart"
-                  onClick={() => {
-                    removeFromCart(productId);
-                  }}
-                >
-                  &times;
-                </button>
-              </div>
-            );
-          })}
-          <p className="total-price">Total price, ${totalPrice.toFixed(2)}</p>
+          <CartItemsList />
+          <p className="total-price">
+            Total price: <span>${totalPrice.toFixed(2)}</span>
+          </p>
         </>
       )}
     </div>
