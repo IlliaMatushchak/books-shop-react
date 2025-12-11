06bec0f5530cc8bde6f258dc8a4062d1f83ca5e8
IlliaMatushchak <coldnight100@gmail.com>
2025-12-11 15:13:08 +0200

    Add ability to set CSS classname to Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,13 @@
 import "./Loader.css";
 
-export default function Loader({ type = "" }) {
+export default function Loader({ className = "", type = "" }) {
   let loader;
+  const containerClassName = "spiner-container " + className;
 
   switch (type) {
     case "global":
       loader = (
-        <div className="spiner-container global">
+        <div className={"global " + containerClassName}>
           <div className="spinner"></div>
           <h2>Loading...</h2>
         </div>
@@ -14,7 +15,7 @@ export default function Loader({ type = "" }) {
       break;
     case "named":
       loader = (
-        <div className="spiner-container">
+        <div className={containerClassName}>
           <div className="spinner"></div>
           <h2>Loading...</h2>
         </div>
@@ -22,14 +23,14 @@ export default function Loader({ type = "" }) {
       break;
     case "small":
       loader = (
-        <div className="spiner-container">
+        <div className={containerClassName}>
           <div className="spinner"></div>
         </div>
       );
       break;
     default:
       loader = (
-        <div className="spiner-container">
+        <div className={containerClassName}>
           <h2>Loading...</h2>
         </div>
       );
