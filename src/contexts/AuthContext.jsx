3fa109b3cf91177fb40395b8e6bbc52d83d4c410
IlliaMatushchak <coldnight100@gmail.com>
2026-01-02 13:18:16 +0200

    Refactor token management in AuthContext

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -9,12 +9,19 @@ import { LocalStorageService, LS_KEYS } from "../services/localStorage";
 
 const AuthContext = createContext(null);
 
+const updateToken = (token = null) => {
+  if (token === null) {
+    LocalStorageService.remove(LS_KEYS.ACCESS_TOKEN);
+    LocalStorageService.remove(LS_KEYS.REFRESH_TOKEN);
+  } else {
+    LocalStorageService.set(LS_KEYS.ACCESS_TOKEN, token.accessToken);
+    LocalStorageService.set(LS_KEYS.REFRESH_TOKEN, token.refreshToken);
+  }
+};
+
 function AuthProvider({ children }) {
   const [user, setUser] = useState(() => LocalStorageService.get(LS_KEYS.USER));
-  const [token, setToken] = useState(() =>
-    LocalStorageService.getRaw(LS_KEYS.TOKEN)
-  );
-  const isLoggedIn = !!token && !!user;
+  const isLoggedIn = !!user;
 
   const updateUser = useCallback((newUser = null) => {
     if (newUser === null) {
@@ -31,16 +38,6 @@ function AuthProvider({ children }) {
     console.error("updateUser: invalid argument", newUser);
   }, []);
 
-  const updateToken = useCallback((token = null) => {
-    if (token === null) {
-      LocalStorageService.remove(LS_KEYS.TOKEN);
-      setToken(null);
-    } else {
-      setToken(token);
-      LocalStorageService.set(LS_KEYS.TOKEN, token);
-    }
-  }, []);
-
   const updateAvatar = useCallback((newAvatar) => {
     setUser((prevUser) => {
       if (!prevUser) return prevUser;
@@ -52,33 +49,28 @@ function AuthProvider({ children }) {
 
   const login = useCallback(
     (userData) => {
-      if (!userData?.token || !userData?.user) {
-        console.error("login: invalid userData", userData);
-        return;
-      }
       const { token, user } = userData;
       updateToken(token);
       updateUser(user);
     },
-    [updateToken, updateUser]
+    [updateUser]
   );
 
   const logout = useCallback(() => {
     updateToken(null);
     updateUser(null);
-  }, [updateToken, updateUser]);
+  }, [updateUser]);
 
   const value = useMemo(
     () => ({
       user,
-      token,
       isLoggedIn,
       login,
       logout,
       updateUser,
       updateAvatar,
     }),
-    [user, token, isLoggedIn, login, logout, updateUser, updateAvatar]
+    [user, isLoggedIn, login, logout, updateUser, updateAvatar]
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
