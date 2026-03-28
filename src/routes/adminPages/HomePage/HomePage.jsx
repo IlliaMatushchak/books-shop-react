a69ecea123a7bf5d5025f1a139d8275dd44f255c
IlliaMatushchak <coldnight100@gmail.com>
2026-03-28 21:08:27 +0200

    feature: extend the HomePage

--- src/routes/adminPages/HomePage/HomePage.jsx
+++ src/routes/adminPages/HomePage/HomePage.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+import { ROUTES } from "../../../constants/routes";
 import "./HomePage.css";
 
 function HomePage() {
@@ -10,6 +12,19 @@ function HomePage() {
         To show the Admin Menu click the <b>"&gt;"</b> button on the left side of the screen. To{" "}
         <b>hide</b> it again, click the <b>"X"</b> button.
       </p>
+      <h3>Currently available pages:</h3>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={ROUTES.ADMIN_BOOKS}>Manage books</NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink to={ROUTES.ADMIN_BOOK_CREATE}>Add new book</NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
